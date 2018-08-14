623be80b6b6490a50c6757583c5e27a8314e71b8
reinaldariel <reinald.a.k@gmail.com>
2018-08-14 13:18:49 +0700

    DB test
    db dummy untuk test

--- banking.sql
+++ banking.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 13, 2018 at 04:45 AM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.2.5
+-- Generation Time: Aug 14, 2018 at 08:17 AM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 5.6.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -29,10 +27,18 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cabang_bank` (
-  `kode_cabang` varchar(30) NOT NULL,
-  `nama_cabang` text NOT NULL,
+  `kode_cabang` varchar(10) NOT NULL,
+  `nama_cabang` varchar(50) NOT NULL,
   `almt_cabang` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `cabang_bank`
+--
+
+INSERT INTO `cabang_bank` (`kode_cabang`, `nama_cabang`, `almt_cabang`) VALUES
+('asd1', 'jogja', 'jogja'),
+('asd2', 'kcp Temanggung', 'Temanggung');
 
 -- --------------------------------------------------------
 
@@ -41,13 +47,26 @@ CREATE TABLE `cabang_bank` (
 --
 
 CREATE TABLE `nasabah` (
-  `id_user` int(20) NOT NULL,
-  `email` int(40) NOT NULL,
-  `nama_lengkap` int(100) NOT NULL,
-  `no_ktp` int(20) NOT NULL,
-  `tgl_lahir` int(20) NOT NULL,
-  `alamat` int(100) NOT NULL
+  `id_nasabah` varchar(5) NOT NULL,
+  `email` varchar(40) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `nama_lengkap` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `no_ktp` varchar(20) NOT NULL,
+  `tgl_lahir` date NOT NULL,
+  `alamat` varchar(100) NOT NULL,
+  `kode_rahasia` varchar(6) NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `nasabah`
+--
+
+INSERT INTO `nasabah` (`id_nasabah`, `email`, `username`, `nama_lengkap`, `password`, `no_ktp`, `tgl_lahir`, `alamat`, `kode_rahasia`, `created`) VALUES
+('1', 'reinald.a.k@gmail.com', 'reinald', 'reinalda ar', '123456', '3323031211960005', '2018-08-06', 'temanggung', '654321', '2018-08-13 09:47:20'),
+('2', 'boni@gmail.com', 'bonii', 'bonifasius', 'boni123', '123513163', '2018-02-05', 'magelang', '112233', '2018-08-13 09:47:20'),
+('3', 'asd@gmail.com', 'coba', 'coba', 'coba', '123', '2018-08-02', 'coba', '123', '2018-08-14 03:53:23');
 
 -- --------------------------------------------------------
 
@@ -56,12 +75,18 @@ CREATE TABLE `nasabah` (
 --
 
 CREATE TABLE `pulsa` (
-  `kode_pembelian` int(100) NOT NULL,
+  `kode_pembelian` varchar(10) NOT NULL,
   `no_hp` varchar(40) NOT NULL,
-  `provider` varchar(20) NOT NULL,
-  `nominal` int(30) NOT NULL,
-  `tgl_pembelian` date NOT NULL
+  `provider` enum('Indosat','XL','Telkomsel','Smartfren') NOT NULL,
+  `nominal` int(6) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pulsa`
+--
+
+INSERT INTO `pulsa` (`kode_pembelian`, `no_hp`, `provider`, `nominal`) VALUES
+('20001', '08978902350', 'Telkomsel', 50000);
 
 -- --------------------------------------------------------
 
@@ -70,11 +95,19 @@ CREATE TABLE `pulsa` (
 --
 
 CREATE TABLE `rekening` (
-  `no_rek` int(100) NOT NULL,
-  `pin` int(40) NOT NULL,
-  `jml_saldo` int(20) NOT NULL,
-  `id_user` int(20) NOT NULL
+  `no_rek` varchar(16) NOT NULL,
+  `jml_saldo` int(11) NOT NULL,
+  `id_nasabah` varchar(5) NOT NULL,
+  `kode_cabang` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `rekening`
+--
+
+INSERT INTO `rekening` (`no_rek`, `jml_saldo`, `id_nasabah`, `kode_cabang`) VALUES
+('039210480', 1000000, '1', 'asd1'),
+('215162637', 2000000, '2', 'asd2');
 
 -- --------------------------------------------------------
 
@@ -83,13 +116,22 @@ CREATE TABLE `rekening` (
 --
 
 CREATE TABLE `transaksi` (
-  `tgl_mulai` datetime NOT NULL,
-  `tgl_sampai` date NOT NULL,
-  `kode_transfer` int(100) NOT NULL,
-  `kode_pembelian` int(100) NOT NULL,
-  `id_user` int(20) NOT NULL,
-  `no_rek` int(100) NOT NULL
+  `kode_transaksi` varchar(20) NOT NULL,
+  `id_nasabah` varchar(3) NOT NULL,
+  `tgl_trans` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `jenis` varchar(2) NOT NULL,
+  `status` varchar(8) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `transaksi`
+--
+
+INSERT INTO `transaksi` (`kode_transaksi`, `id_nasabah`, `tgl_trans`, `jenis`, `status`) VALUES
+('10001', '1', '2018-08-13 08:06:41', 'DB', 'Berhasil'),
+('10002', '2', '2018-08-13 08:06:41', 'DB', 'Berhasil'),
+('10003', '2', '2018-08-13 08:06:41', 'DB', 'Gagal'),
+('20001', '1', '2018-08-13 08:06:41', 'DB', 'Berhasil');
 
 -- --------------------------------------------------------
 
@@ -98,25 +140,20 @@ CREATE TABLE `transaksi` (
 --
 
 CREATE TABLE `transfer` (
-  `kode_transfer` int(100) NOT NULL,
+  `kode_transfer` varchar(100) NOT NULL,
   `rek_transfer` varchar(40) NOT NULL,
   `nominal` int(50) NOT NULL,
-  `keterangan` text NOT NULL,
-  `tgl_transaksi` date NOT NULL
+  `keterangan` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Table structure for table `user`
+-- Dumping data for table `transfer`
 --
 
-CREATE TABLE `user` (
-  `id` int(30) NOT NULL,
-  `username` int(40) NOT NULL,
-  `password` int(20) NOT NULL,
-  `kode_rahasia` int(30) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+INSERT INTO `transfer` (`kode_transfer`, `rek_transfer`, `nominal`, `keterangan`) VALUES
+('10001', '214534', 50000, 'coba'),
+('10002', '87654', 100000, 'coba 2'),
+('10003', '765421', 30000, 'coba 3');
 
 --
 -- Indexes for dumped tables
@@ -132,9 +169,11 @@ ALTER TABLE `cabang_bank`
 -- Indexes for table `nasabah`
 --
 ALTER TABLE `nasabah`
-  ADD PRIMARY KEY (`id_user`),
+  ADD PRIMARY KEY (`id_nasabah`),
   ADD UNIQUE KEY `email` (`email`),
-  ADD KEY `id` (`id_user`);
+  ADD UNIQUE KEY `username_2` (`username`),
+  ADD KEY `username` (`username`),
+  ADD KEY `id_nasabah` (`id_nasabah`);
 
 --
 -- Indexes for table `pulsa`
@@ -147,16 +186,17 @@ ALTER TABLE `pulsa`
 --
 ALTER TABLE `rekening`
   ADD PRIMARY KEY (`no_rek`),
-  ADD KEY `id_nasabah` (`id_user`);
+  ADD KEY `id_nasabah` (`id_nasabah`),
+  ADD KEY `kode_cabang` (`kode_cabang`);
 
 --
 -- Indexes for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD KEY `kode_pembelian` (`kode_pembelian`),
-  ADD KEY `kode_transfer` (`kode_transfer`),
-  ADD KEY `no_rek` (`no_rek`),
-  ADD KEY `transaksi_ibfk_3` (`id_user`);
+  ADD PRIMARY KEY (`kode_transaksi`),
+  ADD KEY `kode_transfer` (`kode_transaksi`),
+  ADD KEY `id_nasabah` (`id_nasabah`),
+  ADD KEY `id_nasabah_2` (`id_nasabah`);
 
 --
 -- Indexes for table `transfer`
@@ -165,58 +205,33 @@ ALTER TABLE `transfer`
   ADD PRIMARY KEY (`kode_transfer`);
 
 --
--- Indexes for table `user`
---
-ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `pulsa`
---
-ALTER TABLE `pulsa`
-  MODIFY `kode_pembelian` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `transfer`
---
-ALTER TABLE `transfer`
-  MODIFY `kode_transfer` int(100) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `user`
---
-ALTER TABLE `user`
-  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT;
-
---
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `nasabah`
+-- Constraints for table `pulsa`
 --
-ALTER TABLE `nasabah`
-  ADD CONSTRAINT `nasabah_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+ALTER TABLE `pulsa`
+  ADD CONSTRAINT `pulsa_ibfk_1` FOREIGN KEY (`kode_pembelian`) REFERENCES `transaksi` (`kode_transaksi`);
 
 --
 -- Constraints for table `rekening`
 --
 ALTER TABLE `rekening`
-  ADD CONSTRAINT `rekening_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`);
+  ADD CONSTRAINT `rekening_ibfk_1` FOREIGN KEY (`kode_cabang`) REFERENCES `cabang_bank` (`kode_cabang`),
+  ADD CONSTRAINT `rekening_ibfk_2` FOREIGN KEY (`id_nasabah`) REFERENCES `nasabah` (`id_nasabah`);
 
 --
 -- Constraints for table `transaksi`
 --
 ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`kode_pembelian`) REFERENCES `pulsa` (`kode_pembelian`),
-  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`kode_transfer`) REFERENCES `transfer` (`kode_transfer`),
-  ADD CONSTRAINT `transaksi_ibfk_3` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
-  ADD CONSTRAINT `transaksi_ibfk_4` FOREIGN KEY (`no_rek`) REFERENCES `rekening` (`no_rek`);
-COMMIT;
+  ADD CONSTRAINT `transaksi_ibfk_2` FOREIGN KEY (`id_nasabah`) REFERENCES `nasabah` (`id_nasabah`);
+
+--
+-- Constraints for table `transfer`
+--
+ALTER TABLE `transfer`
+  ADD CONSTRAINT `transfer_ibfk_1` FOREIGN KEY (`kode_transfer`) REFERENCES `transaksi` (`kode_transaksi`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
