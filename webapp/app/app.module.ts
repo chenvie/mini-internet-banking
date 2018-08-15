cf037df95a9eebf4ccf6e169e91d8ec80e67fa57
Dariel Lambert <dariel.lambet@ti.ukdw.ac.id>
2018-08-15 10:41:45 +0700

    penambahan UI

--- webapp/app/app.module.ts
+++ webapp/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { LoginScreenComponent } from './login-screen/login-screen.component';
@@ -29,8 +28,7 @@ import { SettingComponent } from './setting/setting.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpClientModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
