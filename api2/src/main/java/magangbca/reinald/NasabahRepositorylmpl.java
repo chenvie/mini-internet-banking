555526a7893e16712119cefc3876fc106e461773
Vievin Efendy <vievin.efendy@ti.ukdw.ac.id>
2018-09-18 14:13:20 +0700

    Auto stash before revert of "change tomcat server port to 8888"

--- api2/src/main/java/magangbca/reinald/NasabahRepositorylmpl.java
+++ api2/src/main/java/magangbca/reinald/NasabahRepositorylmpl.java
@@ -13,21 +13,21 @@ public class NasabahRepositorylmpl{
 
     public List<String> updatePassword(int id_nasabah, String passwordl, String passwordb1, String passwordb2) {
         StoredProcedureQuery query = entityManager.createStoredProcedureQuery("putNasabahPassword")
-                .registerStoredProcedureParameter(1, int.class, ParameterMode.IN)
-                .registerStoredProcedureParameter(2, String.class, ParameterMode.IN)
-                .registerStoredProcedureParameter(3, String.class, ParameterMode.IN)
-                .registerStoredProcedureParameter(4, String.class, ParameterMode.IN)
-                .registerStoredProcedureParameter(5, String.class, ParameterMode.OUT)
-                .registerStoredProcedureParameter(6, String.class, ParameterMode.OUT);
+                .registerStoredProcedureParameter("id", int.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("pl", String.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("pb1", String.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("pb2", String.class, ParameterMode.IN)
+                .registerStoredProcedureParameter("sts", String.class, ParameterMode.OUT)
+                .registerStoredProcedureParameter("msg", String.class, ParameterMode.OUT);
 
-        query.setParameter(1, id_nasabah)
-                .setParameter(2, passwordl)
-                .setParameter(3, passwordb1)
-                .setParameter(4, passwordb2);
+        query.setParameter("id", id_nasabah)
+                .setParameter("pl", passwordl)
+                .setParameter("pb1", passwordb1)
+                .setParameter("pb2", passwordb2);
 
         query.execute();
-        String status = query.getOutputParameterValue("stts").toString();
-        String message = query.getOutputParameterValue("msg").toString();
+        String status = query.getOutputParameterValue("5").toString();
+        String message = query.getOutputParameterValue("6").toString();
 
         List<String> output = new ArrayList<String>();
         output.add(status);
