e0b502017f2edccb911a452ffd55709b00215da7
reinaldariel <reinald.a.k@gmail.com>
2018-09-19 16:19:46 +0700

    workspace

--- api2/src/main/java/magangbca/reinald/NasabahRepositorylmpl.java
+++ api2/src/main/java/magangbca/reinald/NasabahRepositorylmpl.java
@@ -3,9 +3,7 @@ package magangbca.reinald;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.*;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Repository
