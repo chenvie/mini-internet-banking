13c06ecc329135b766f621b891f19198b88f83e6
Vievin Efendy <vievin.efendy@ti.ukdw.ac.id>
2018-10-01 16:49:32 +0700

    adding instrumented test open drawer layout (still not done)

--- mobile-app/app/src/androidTest/java/bca/co/id/mini_internet_banking/BalanceInfoTest.java
+++ mobile-app/app/src/androidTest/java/bca/co/id/mini_internet_banking/BalanceInfoTest.java
@@ -1,6 +1,9 @@
 package bca.co.id.mini_internet_banking;
 
+import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.DrawerActions;
 import android.support.test.rule.ActivityTestRule;
+import android.view.Gravity;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -8,7 +11,9 @@ import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class BalanceInfoTest {
 
@@ -22,6 +27,8 @@ public class BalanceInfoTest {
 
     @Test
     public void showBalance() throws Exception{
-        onView(withId(R.id.menu_balance)).perform(click());
+        //onView(withId(R.id.menu_balance)).perform(click());
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.nav_balance));
     }
 }
