e784a7ef6ae63edbebcb2b157b5a2cc9edc9fbc6
Vievin Efendy <vievin.efendy@ti.ukdw.ac.id>
2018-10-19 14:04:44 +0700

    PWA Example Deploy to Github Pages

---
+++ PWCat/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { MatToolbarModule, MatCardModule } from '@angular/material';
+import { ImgCardComponent } from './img-card/img-card.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ImgCardComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatToolbarModule,
+    MatCardModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
